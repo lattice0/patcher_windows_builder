73281c1c8c236bf16c9bb3d98af96dfb1b2c5842
Lattice 0 <lattice0@protonmail.com>
2022-08-09 20:50:19 -0300

    isntal

--- scripts/install_deps.ps1
+++ scripts/install_deps.ps1
@@ -1,7 +1,7 @@
 Write-Host "Installing deps."
 
 
-# Rust installation
+# ---- Rust installation
 Write-Host "Installing Rust..." -ForegroundColor Cyan
 $exePath = "$env:TEMP\rustup-init.exe"
 
@@ -19,3 +19,15 @@ rustup --version
 rustc --version
 
 Write-Host "Rust installed" -ForegroundColor Green
+# ---- End rust installation
+
+# Cmake
+
+Write-Host "Installing Cmake..." -ForegroundColor Cyan
+$exePath = "$env:TEMP\cmake.exe"
+
+Write-Host "Downloading..."
+(New-Object Net.WebClient).DownloadFile('https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0-windows-x86_64.msi', $exePath)
+
+Write-Host "Installing..."
+MsiExec.exe /i $exePath ADD_CMAKE_TO_PATH=User /qn
