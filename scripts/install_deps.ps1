351551edc7023bd6bdff623011d6919a5c6d462b
Lattice 0 <lattice0@protonmail.com>
2022-08-08 22:56:52 -0300

    first commit

---
+++ scripts/install_deps.ps1
@@ -0,0 +1,21 @@
+Write-Host "Installing deps."
+
+
+# Rust installation
+Write-Host "Installing Rust..." -ForegroundColor Cyan
+$exePath = "$env:TEMP\rustup-init.exe"
+
+Write-Host "Downloading..."
+(New-Object Net.WebClient).DownloadFile('https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe', $exePath)
+
+Write-Host "Installing..."
+cmd /c start /wait $exePath -y
+Remove-Item $exePath
+
+$env:Path = "$env:USERPROFILE\.cargo\bin"
+
+cargo --version
+rustup --version
+rustc --version
+
+Write-Host "Rust installed" -ForegroundColor Green
