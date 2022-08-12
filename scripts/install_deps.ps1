beb5e36125f063c778b66a61ba00bf202c82da5e
Lattice 0 <lattice0@protonmail.com>
2022-08-11 21:45:41 -0300

    cmake works

--- scripts/install_deps.ps1
+++ scripts/install_deps.ps1
@@ -10,7 +10,7 @@ Write-Host "Downloading..."
 Write-Host "Unzipping..."
 Expand-Archive -Path "$zipFile" -DestinationPath "C:\src\flutter" -Verbose
 
-$env:Path = "$env:C:\src\flutter"
+[Environment]::SetEnvironmentVariable('PATH', 'C:\src\flutter\flutter\bin;' + $env:PATH, 'Machine')
 
 flutter --version
 
@@ -22,23 +22,43 @@ Write-Host "Downloading..."
 (New-Object Net.WebClient).DownloadFile('https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0-windows-x86_64.msi', $exePath)
 
 Write-Host "Installing..."
-Start-Process msiexec.exe -Wait -ArgumentList "/i $exePath ADD_CMAKE_TO_PATH=User /n"
+#msiexec.exe /i $exePath ADD_CMAKE_TO_PATH=User /qn | Out-Null
+Start-Process msiexec.exe -ArgumentList "/i $exePath ADD_CMAKE_TO_PATH=User /qn" -Wait
+
+foreach($level in "Machine","User") {
+   [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+      # For Path variables, append the new values, if they're not already in there
+      if($_.Name -match 'Path$') { 
+         $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+      }
+      $_
+   } | Set-Content -Path { "Env:$($_.Name)" }
+}
 
 cmake --version
 
-
 # ---- Git For Windows Installation
 Write-Host "Installing Git for windows..." -ForegroundColor Cyan
-$exePath = "$env:TEMP\git.msi"
+$exePath = "$env:TEMP\git.exe"
 
 Write-Host "Downloading..."
 (New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.37.1.windows.1/Git-2.37.1-64-bit.exe', $exePath)
 
 Write-Host "Installing..."
-Start-Process msiexec.exe -Wait -ArgumentList '$exePath /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh" /LOG="C:git-for-windows.log"'
+Start-Process $exePath -Wait -ArgumentList '/NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh" /LOG="C:\git-for-windows.log"'
+
+foreach($level in "Machine","User") {
+   [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+      # For Path variables, append the new values, if they're not already in there
+      if($_.Name -match 'Path$') { 
+         $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+      }
+      $_
+   } | Set-Content -Path { "Env:$($_.Name)" }
+}
 
 git --version
-bash
+bash --version
 
 # ---- Rust installation
 Write-Host "Installing Rust..." -ForegroundColor Cyan
@@ -51,7 +71,8 @@ Write-Host "Installing..."
 cmd /c start /wait $exePath -y
 Remove-Item $exePath
 
-$env:Path = "$env:USERPROFILE\.cargo\bin"
+#$env:Path = "$env:USERPROFILE\.cargo\bin"
+[Environment]::SetEnvironmentVariable('PATH', $env:PATH, "USERPROFILE\.cargo\bin")
 
 cargo --version
 rustup --version
