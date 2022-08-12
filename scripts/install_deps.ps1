b2a4050421545915d5b383cda69f2644acb70f80
Lattice 0 <lattice0@protonmail.com>
2022-08-12 17:33:01 -0300

    flutter and rust reload path works

--- scripts/install_deps.ps1
+++ scripts/install_deps.ps1
@@ -1,5 +1,18 @@
 Write-Host "Installing deps."
 
+# Make new environment variables available in the current PowerShell session:
+function reload {
+   foreach($level in "Machine","User") {
+      [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+         # For Path variables, append the new values, if they're not already in there
+         if($_.Name -match 'Path$') { 
+            $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+         }
+         $_
+      } | Set-Content -Path { "Env:$($_.Name)" }
+   }
+}
+
 # ---- Flutter installation
 Write-Host "Installing Flutter..." -ForegroundColor Cyan
 $zipFile = "$env:TEMP\flutter.zip"
@@ -10,7 +23,11 @@ Write-Host "Downloading..."
 Write-Host "Unzipping..."
 Expand-Archive -Path "$zipFile" -DestinationPath "C:\src\flutter" -Verbose
 
-[Environment]::SetEnvironmentVariable('PATH', 'C:\src\flutter\flutter\bin;' + $env:PATH, 'Machine')
+[Environment]::SetEnvironmentVariable('Path', "$([Environment]::GetEnvironmentVariable('Path', 'Machine'));C:\src\flutter\flutter\bin", 'Machine')
+
+reload
+
+$env:PATH -split ';'
 
 flutter --version
 
@@ -25,37 +42,22 @@ Write-Host "Installing..."
 #msiexec.exe /i $exePath ADD_CMAKE_TO_PATH=User /qn | Out-Null
 Start-Process msiexec.exe -ArgumentList "/i $exePath ADD_CMAKE_TO_PATH=User /qn" -Wait
 
-foreach($level in "Machine","User") {
-   [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
-      # For Path variables, append the new values, if they're not already in there
-      if($_.Name -match 'Path$') { 
-         $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
-      }
-      $_
-   } | Set-Content -Path { "Env:$($_.Name)" }
-}
+reload
 
 cmake --version
 
-# ---- Git For Windows Installation
-Write-Host "Installing Git for windows..." -ForegroundColor Cyan
 $exePath = "$env:TEMP\git.exe"
 
-Write-Host "Downloading..."
-(New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.37.1.windows.1/Git-2.37.1-64-bit.exe', $exePath)
+# Download git installer
+Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.37.1.windows.1/Git-2.37.1-64-bit.exe -UseBasicParsing -OutFile $exePath
 
-Write-Host "Installing..."
-Start-Process $exePath -Wait -ArgumentList '/NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh" /LOG="C:\git-for-windows.log"'
+# Execute git installer
+Start-Process $exePath -ArgumentList '/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"' -Wait
 
-foreach($level in "Machine","User") {
-   [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
-      # For Path variables, append the new values, if they're not already in there
-      if($_.Name -match 'Path$') { 
-         $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
-      }
-      $_
-   } | Set-Content -Path { "Env:$($_.Name)" }
-}
+# Optional: For bash.exe, add 'C:\Program Files\Git\bin' to PATH
+[Environment]::SetEnvironmentVariable('Path', "$([Environment]::GetEnvironmentVariable('Path', 'Machine'));C:\Program Files\Git\bin", 'Machine')
+
+reload
 
 git --version
 bash --version
@@ -71,11 +73,24 @@ Write-Host "Installing..."
 cmd /c start /wait $exePath -y
 Remove-Item $exePath
 
-#$env:Path = "$env:USERPROFILE\.cargo\bin"
-[Environment]::SetEnvironmentVariable('PATH', $env:PATH, "USERPROFILE\.cargo\bin")
+$addPath = "$env:USERPROFILE\.cargo\bin"
+[Environment]::SetEnvironmentVariable
+     ($addPath, $env:Path, [System.EnvironmentVariableTarget]::Machine)
+
+reload
 
 cargo --version
 rustup --version
 rustc --version
 
-Write-Host "Rust installed" -ForegroundColor Green
+# VCPKG
+
+git clone https://github.com/Microsoft/vcpkg.git C:\src\vcpkg
+
+C:\src\vcpkg\bootstrap-vcpkg.bat
+
+C:\src\vcpkg\vcpkg.exe install gtest
+C:\src\vcpkg\vcpkg.exe install ffmpeg
+
+Get-ChildItem C:\src\vcpkg
+
